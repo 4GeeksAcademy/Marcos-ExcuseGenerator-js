e0c77bf1725c2b97df589689018ac839e4599360
Marcos Cabral Peralta <156668187+Marcos-93LAB@users.noreply.github.com>
2024-03-04 20:31:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generadorExcusa();
+  });
+};
+
+let generadorExcusa = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoindex = Math.floor(Math.random() * who.length);
+  let actionindex = Math.floor(Math.random() * action.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+  let whenindex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoindex] +
+    " " +
+    action[actionindex] +
+    " " +
+    what[whatindex] +
+    " " +
+    when[whenindex]
+  );
 };
